53fcffbc2ded137a047977af17e992bce85dfeea
Liplum <Li_plum@outlook.com>
2023-03-06 11:06:38 +0800

    permission

--- src/auth.ts
+++ src/auth.ts
@@ -4,3 +4,27 @@ export enum AuthError {
   wrongStudentIdOrPassword = "wrongStudentIdOrPassword",
   staffNotFound = "staffNotFound",
 }
+
+export enum StaffPermission {
+  /**
+   * To add, remove and update a staff.
+   * Staff can natually alter themselves.
+   */
+  alterStaffs = "alterStaffs",
+  /**
+   * To create and submit a transaction and a rental. 
+   */
+  submitTransaction = "submitTransaction",
+  /**
+   * To directly change one's point.
+   */
+  alterPoint = "alterPoint",
+  /**
+   * To update the info of students, such as name.
+   */
+  alterStudentInfo = "alterStudentInfo",
+  /**
+   * To update the detail of students, such as phone number. 
+   */
+  alterStudentDetail = "alterStudentDetail",
+}
