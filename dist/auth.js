09512e97d936c5e003b80233bf1751ae46d4fdbe
Liplum <Li_plum@outlook.com>
2023-03-06 08:45:50 +0800

    dist

---
+++ dist/auth.js
@@ -0,0 +1,7 @@
+export var AuthError;
+(function (AuthError) {
+    AuthError["missingHeader"] = "missingAuthHeader";
+    AuthError["invalidJwt"] = "invalidJwt";
+    AuthError["wrongStudentIdOrPassword"] = "wrongStudentIdOrPassword";
+})(AuthError || (AuthError = {}));
+//# sourceMappingURL=auth.js.map
